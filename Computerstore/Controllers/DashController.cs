3cd08ea7a9099d4a7c8041b9f4b62ff3c1619ce7
Bibek2000 <bibekluitel2000@gmail.com>
2022-07-05 15:50:49 +0545

    first commit

---
+++ Computerstore/Controllers/DashController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Computerstore.Controllers
+{
+    public class DashController : Controller
+    {
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+        public ViewResult AddComputer()
+        {
+            return View();
+        }
+    }
+}
